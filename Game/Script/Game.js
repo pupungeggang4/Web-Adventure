3e8ad5103f314cccbf7371c45fdb43e037234f84
pupungeggang6 <pupungeggang6@gmail.com>
2025-05-02 13:20:11 +0900

    Temp

---
+++ Game/Script/Game.js
@@ -0,0 +1,7 @@
+let game
+
+class Game {
+    constructor() {
+
+    }
+}
--- Game/Script/Main.js
+++ Game/Script/Main.js
@@ -3,11 +3,13 @@ window.onerror = errorHandle
 window.oncontextmenu = rightClick
 
 function main() {
-
+    game = new Game()
 }
 
 function errorHandle(err, url, line, col, obj) {
-
+    if (obj != null) {
+        cancelAnimationFrame(game.gameLoop)
+    }
 }
 
 function rightClick() {
